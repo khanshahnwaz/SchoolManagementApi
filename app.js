28e5b6c0222aade7d34e56fef71a8afbdcda9a89
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2024-08-23 08:39:54 +0530

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
